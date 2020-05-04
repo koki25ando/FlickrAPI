325532cf9048eb21d2ad4631eff83902847e7a96
koki25ando <basket.koki.bowl@gmail.com>
2020-05-04 11:46:10 +0900

    Changed output column name, Added external reference

--- R/getHotTags.R
+++ R/getHotTags.R
@@ -1,40 +1,39 @@
 #' Returns a list of hot tags for the given period.
 #'
 #' @param api_key Your API application key
-#' @param period The period for which to fetch hot tags. Valid values are day and week. Defaults to day
+#' @param period The period for which to fetch hot tags. Valid values are day or week. Defaults to day
 #' @param count The number of tags to return. Defaults to 20. Maximum allowed value is 200.
-#' 
+#'
 #' @author Koki Ando
-#' 
+#'
 #' @import RCurl
 #' @import jsonlite
 #' @import stringr
-#' 
+#' @import magrittr
+#'
+#' @seealso \url{https://www.flickr.com/services/api/flickr.tags.getHotList.html}
+#'
 #' @return This function a data frame of hot tags for the given period
-#' 
+#'
 #' @examples
 #' \dontrun{
-#'  getHotTags(api_key = "********************************", 
+#'  getHotTags(api_key = "*********",
 #'             period = "day", count=20)
 #' }
 #'
 #' @export
 
-getHotTags <- function(api_key, period = "day", count=20){
+getHotTags <- function(api_key, period = c("day", "week"), count=20){
   head_url = "https://api.flickr.com/services/rest/?method=flickr.tags.getHotList&api_key="
   url = paste0(head_url, api_key, "&period=",
-               stringr::str_to_lower(period), "&count=", count, 
+               stringr::str_to_lower(period), "&count=", count,
                "&format=json&nojsoncallback=1")
-  raw_data <- RCurl::getURL(url, ssl.verifypeer = FALSE)
-  data <- jsonlite::fromJSON(raw_data)
-  data$hottags$tag
+  data = RCurl::getURL(url, ssl.verifypeer = FALSE) %>%
+    jsonlite::fromJSON()
+  hot_tags_df = data$hottags$tag
+  names(hot_tags_df)[2] = 'tag'
+  hot_tags_df
 }
-
-
-
-
-
-
 
 
 
