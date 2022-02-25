9b5f91ad9a5ce2b0c9cba833688408063fdfdc88
Eli Pousson <eli.pousson@gmail.com>
2022-02-24 20:53:49 -0500

    feat: correct license_id parameter name to match API spelling
    - feat: correct license_id parameter name to match API spelling (retains licence_id parameter for backward compatibility)
    - refactor: use match.arg to validate sort and extras parameters

--- R/getPhotoSearch.R
+++ R/getPhotoSearch.R
@@ -1,6 +1,8 @@
 #' Search for photos on Flickr by user id, tags, license, or bounding box
 #'
-#' R access to Flickr search API.
+#' Use the Flickr Search API to return pages of photos sorted by date posted,
+#' date taken, interestingness, and relevance. Optional search parameters
+#' including spatial bounding box, user id, tags, and image license.
 #'
 #' @param api_key Flickr API key. If api_key is `NULL`, the function uses
 #'   [getFlickrAPIKey()] to use the environment variable "FLICKR_API_KEY" as the
@@ -8,7 +10,7 @@
 #' @param user_id The NSID of the user who's photo to search. If this parameter
 #'   isn't passed then everybody's public photos will be searched.
 #' @param tags A vector of tags you wish to search for.
-#' @param license_id The license id for photos (for possible values see the
+#' @param license_id,licence_id The license id for photos (for possible values see the
 #'   Flickr API method flickr.photos.licenses.getInfo). license_id can be an
 #'   integer from 0 to 10 or a corresponding license code including "c" (All
 #'   Rights Reserved), "by-bc-sa" (Attribution-NonCommercial-ShareAlike),
@@ -66,53 +68,58 @@
 #' )
 #' }
 #' @export
+#' @importFrom utils URLencode
 #' @importFrom RCurl getURL
 #' @importFrom jsonlite fromJSON
 
 getPhotoSearch <- function(api_key = NULL,
                            user_id = NULL,
                            tags = NULL,
-                           licence_id = NULL,
+                           license_id = NULL,
+                           licence_id,
                            sort = "date-posted-asc",
                            bbox = NULL,
-                           extras,
+                           extras = NULL,
                            per_page = 100,
                            page = 1) {
   api_key <- getFlickrAPIKey(api_key)
 
   url <-
-      paste0(
-        "https://api.flickr.com/services/rest/",
-        "?method=flickr.photos.search",
-        "&api_key=", api_key,
-        "&user_id=", user_id,
-        "&tags=", paste(tags, collapse = ",")
-      )
+    paste0(
+      "https://api.flickr.com/services/rest/",
+      "?method=flickr.photos.search",
+      "&api_key=", api_key,
+      "&user_id=", user_id,
+      "&tags=", paste(tags, collapse = ",")
+    )
 
-  if (!is.null(licence_id)) {
-    if (is.character(licence_id)) {
+  if (!missing(licence_id)) {
+    license_id <- licence_id
+  }
+
+  if (!is.null(license_id)) {
+    if (is.character(license_id)) {
       license_id <- switch(license_id,
-                           "c" = 0,
-                           "by-bc-sa" = 1,
-                           "by-nc" = 2,
-                           "by-nc-nd" = 3,
-                           "by" = 4,
-                           "by-sa" = 5,
-                           "by-nd" = 6,
-                           "nkc" = 7,
-                           "pd-us" = 8,
-                           "cc0" = 9,
-                           "pd" = 10
+        "c" = 0,
+        "by-bc-sa" = 1,
+        "by-nc" = 2,
+        "by-nc-nd" = 3,
+        "by" = 4,
+        "by-sa" = 5,
+        "by-nd" = 6,
+        "nkc" = 7,
+        "pd-us" = 8,
+        "cc0" = 9,
+        "pd" = 10
       )
-    } else if (!(licence_id %in% c(0:10))) {
+    } else if (!(license_id %in% c(0:10))) {
       stop("The license id provided is not valid. The license id must be an integer from 0 to 10.")
     }
   }
+  url <- paste0(url, "&license=", license_id)
 
-  url <- paste0(url, "&license=", licence_id)
-
-  if (!(sort %in% c("date-posted-asc", "date-posted-desc", "date-taken-asc", "date-taken-desc", "interestingness-desc", "interestingness-asc", "relevance"))) {
-    stop("The sort provided is not valid. Possible sort values are: date-posted-asc, date-posted-desc, date-taken-asc, date-taken-desc, interestingness-desc, interestingness-asc, and relevance.")
+  if (!is.null(sort)) {
+    sort <- match.arg(sort, c("date-posted-asc", "date-posted-desc", "date-taken-asc", "date-taken-desc", "interestingness-desc", "interestingness-asc", "relevance"))
   }
 
   url <- paste0(url, "&sort=", sort)
@@ -126,15 +133,11 @@ getPhotoSearch <- function(api_key = NULL,
     }
   }
 
-  if (!missing(extras)) {
+  if (!is.null(extras)) {
     extra_fields <- c("description", "license", "date_upload", "date_taken", "owner_name", "icon_server", "original_format", "last_update", "geo", "tags", "machine_tags", "o_dims", "views", "media", "path_alias", "url_sq", "url_t", "url_s", "url_q", "url_m", "url_n", "url_z", "url_c", "url_l", "url_o")
 
     # Check if all elements of extras are valid extra fields
-    if (sum(vapply(extras, function(x) {
-      x %in% extra_fields
-    }, logical(1))) < length(extras)) {
-      stop(paste0("One or more of the extra fields provided is not valid or supported. Supported extra fields are: ", paste0(extra_fields, collapse = ", ")))
-    }
+    extras <- match.arg(extras, extra_fields, several.ok = TRUE)
 
     url <- paste0(url, "&extras=", paste0(extras, collapse = ","))
   }
@@ -160,4 +163,4 @@ getPhotoSearch <- function(api_key = NULL,
 
 #' @export
 #' @rdname getPhotoSearch
-get_photo_search = getPhotoSearch
+get_photo_search <- getPhotoSearch
