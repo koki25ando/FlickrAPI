1d374aad5262f8c30398970e649a6aee095e5d3f
Eli Pousson <eli.pousson@gmail.com>
2022-03-24 12:22:58 -0400

    feat: add img_size and desc parameters to get_photo_search
    - feat: add img_size and desc parameters to get_photo_search while adding ... parameters to support geo and legacy licence_id parameters

--- R/getPhotoSearch.R
+++ R/getPhotoSearch.R
@@ -8,21 +8,22 @@
 #' @param user_id The NSID of the user who's photo to search. If this parameter
 #'   isn't passed then everybody's public photos will be searched.
 #' @param tags A vector of tags you wish to search for.
-#' @param license_id,licence_id The license id for photos (for possible values see the
-#'   Flickr API method flickr.photos.licenses.getInfo). license_id can be an
-#'   integer from 0 to 10 or a corresponding license code including "c" (All
-#'   Rights Reserved), "by-bc-sa" (Attribution-NonCommercial-ShareAlike),
-#'   "by-nc" (Attribution-NonCommercial), "by-nc-nd"
-#'   (Attribution-NonCommercial-NoDerivs), "by" (Attribution), "by-sa"
-#'   (Attribution-ShareAlike), "by-nd" (Attribution-NoDerivs), "nkc" (No known
-#'   copyright restrictions), "pd-us" (United States Government Work), "cc0"
-#'   (Public Domain Dedication), or "pd" (Public Domain Mark).
+#' @param license_id The license id for photos (for possible values see the
+#'   Flickr API method flickr.photos.licenses.getInfo). See details for more information.
 #' @param sort Order to sort returned photos. The possible values are:
 #'   "date-posted-asc", "date-posted-desc", "date-taken-asc", "date-taken-desc",
-#'   "interestingness-desc", "interestingness-asc", and "relevance"
+#'   "interestingness-desc", "interestingness-asc", and "relevance" The trailing
+#'   "-asc" or "-desc" indicator for sort direction is optional when using the
+#'   desc parameter.
+#' @param desc If TRUE, sort in descending order by the selected sort variable;
+#'   defaults to `FALSE`.
 #' @param bbox A object of class `bbox` or a numeric vector with values for
 #'   xmin, ymin, xmax and ymax representing the bottom-left corner of the box
 #'   and the top-right corner.
+#' @param img_size A character string with the abbreviation for one or more
+#'   image sizes ("sq", "t", "s", "q", "m", "n", "z", "c", "l", or "o"). If a
+#'   single img_size is provided the url, width, and height columns are renamed
+#'   (e.g. img_url instead of url_sq) and an img_asp column is added to the results.
 #' @param extras A vector of extra information to fetch for each returned
 #'   record. Currently supported fields are: c("description", "license",
 #'   "date_upload", "date_taken", "owner_name", "icon_server",
@@ -34,8 +35,26 @@
 #'   otherwise.
 #' @param page Number specifying which search results page to return. Default is
 #'   page 1 of results returned.
+#' @param ... Additional parameters that can include licence_id (legacy spelling),
 #' @return This function returns data of specific photos matching search
 #'   parameters.
+#'
+#' @details License id options:
+#'
+#' license_id can be an integer from 0 to 10 or a corresponding license code
+#' including:
+#'
+#' - "c" (All Rights Reserved),
+#' - "by-bc-sa" (Attribution-NonCommercial-ShareAlike),
+#' - "by-nc" (Attribution-NonCommercial),
+#' - "by-nc-nd" (Attribution-NonCommercial-NoDerivs),
+#' - "by" (Attribution),
+#' - "by-sa" (Attribution-ShareAlike),
+#' - "by-nd" (Attribution-NoDerivs),
+#' - "nkc" (No known copyright restrictions),
+#' - "pd-us" (United States Government Work),
+#' - "cc0" (Public Domain Dedication),
+#' - or "pd" (Public Domain Mark).
 #'
 #' @examples
 #' \dontrun{
@@ -66,18 +85,23 @@
 #' )
 #' }
 #' @export
+#' @importFrom rlang list2
 
 getPhotoSearch <- function(api_key = NULL,
                            user_id = NULL,
                            tags = NULL,
                            license_id = NULL,
-                           licence_id,
-                           sort = "date-posted-asc",
+                           sort = "date-posted",
+                           desc = FALSE,
                            bbox = NULL,
+                           img_size = "s",
                            extras = NULL,
                            per_page = 100,
-                           page = 1) {
-  if (!missing(licence_id)) {
+                           page = 1,
+                           ...) {
+  params <- rlang::list2(...)
+
+  if (!is.null(params$licence_id)) {
     license_id <- licence_id
   }
 
@@ -102,14 +126,17 @@ getPhotoSearch <- function(api_key = NULL,
   }
 
   if (!is.null(sort)) {
-    sort <- match.arg(sort, c("date-posted-asc", "date-posted-desc", "date-taken-asc", "date-taken-desc", "interestingness-desc", "interestingness-asc", "relevance"))
-  }
+    if (!grepl("-desc$|-asc$", sort)) {
+      if (desc) {
+        dir <- "-desc"
+      } else {
+        dir <- "-asc"
+      }
 
-  if (!is.null(bbox)) {
-    if (("bbox" %in% class(bbox)) || ((length(bbox) == 4) && is.numeric(bbox))) {
-      bbox <- paste0(bbox, collapse = ",")
+      sort <-
+        match.arg(sort, c(paste0(c("date-posted", "date-taken", "interestingness"), dir), "relevance"))
     } else {
-      stop("The bbox provided is not valid. The bbox must be an object of class 'bbox' or a numeric vector with xmin, ymin, xmax and ymax values.")
+      sort <- match.arg(sort, c("date-posted-asc", "date-posted-desc", "date-taken-asc", "date-taken-desc", "interestingness-desc", "interestingness-asc", "relevance"))
     }
   }
 
@@ -118,6 +145,32 @@ getPhotoSearch <- function(api_key = NULL,
 
     # Check if all elements of extras are valid extra fields
     extras <- match.arg(extras, extra_fields, several.ok = TRUE)
+  }
+
+  if (!is.null(params$geo) && params$geo) {
+    # Always include geo in the extras if geo is TRUE
+    extras <- unique(c(extras, "geo"))
+  }
+
+  # Add url of select size to extras if img_size is provided
+  if (!is.null(img_size)) {
+    img_size <-
+      match.arg(
+        params$img_size,
+        c("sq", "t", "s", "q", "m", "n", "z", "c", "l", "o"),
+        several.ok = TRUE
+      )
+
+    extras <-
+      unique(c(extras, paste0("url_", img_size)))
+  }
+
+  if (!is.null(bbox)) {
+    if (("bbox" %in% class(bbox)) || ((length(bbox) == 4) && is.numeric(bbox))) {
+      bbox <- paste0(bbox, collapse = ",")
+    } else {
+      stop("The bbox provided is not valid. The bbox must be an object of class 'bbox' or a numeric vector with xmin, ymin, xmax and ymax values.")
+    }
   }
 
   data <-
@@ -134,7 +187,24 @@ getPhotoSearch <- function(api_key = NULL,
       extras = paste0(extras, collapse = ",")
     )
 
-  return(as.data.frame(data$photos$photo))
+  data <- as.data.frame(data$photos$photo)
+
+  if (!is.null(img_size) && (length(img_size) == 1)) {
+    url_col <- paste0("url_", img_size)
+    width_col <- paste0("width_", img_size)
+    height_col <- paste0("height_", img_size)
+
+    names(data)[names(data) == url_col] <- "img_url"
+    names(data)[names(data) == width_col] <- "img_width"
+    names(data)[names(data) == height_col] <- "img_height"
+
+    data <-
+      within(data, {
+        img_asp <- img_width / img_height
+      })
+  }
+
+  return(data)
 }
 
 
@@ -142,3 +212,7 @@ getPhotoSearch <- function(api_key = NULL,
 #' @export
 #' @rdname getPhotoSearch
 get_photo_search <- getPhotoSearch
+
+
+utils::globalVariables(c("img_height", "img_width", "licence_id"))
+
