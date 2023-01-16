a9f332519c3cdef9e67a344749e4e76e687d313b
Eli Pousson <eli.pousson@gmail.com>
2023-01-16 17:40:20 -0500

    refactor: drop magrittr from Imports + sf from Suggests

--- R/FlickrAPIRequest.R
+++ R/FlickrAPIRequest.R
@@ -12,50 +12,43 @@
 #' @param check_type Default to `FALSE`, passed to [httr2::resp_body_json]
 #' @param ... Additional parameters passed to [httr2::req_url_query]
 #' @export
-FlickrAPIRequest <-
-  function(method = NULL,
-           api_key = NULL,
-           format = "json",
-           simplifyVector = TRUE,
-           check_type = FALSE,
-           ...) {
-    api_key <- getFlickrAPIKey(api_key)
+#' @importFrom httr2 request req_url_query req_perform resp_body_json
+#'   resp_body_string resp_body_raw
+FlickrAPIRequest <- function(method = NULL,
+                             api_key = NULL,
+                             format = "json",
+                             simplifyVector = TRUE,
+                             check_type = FALSE,
+                             ...) {
+  api_key <- getFlickrAPIKey(api_key)
 
-    req <-
-      httr2::request("https://api.flickr.com/services/rest") %>%
-      httr2::req_url_query(
-        method = method,
-        api_key = api_key
-      )
+  req <- httr2::request("https://api.flickr.com/services/rest")
 
-    req <-
-      httr2::req_url_query(
-        req,
-        ...
-      ) %>%
-      httr2::req_url_query(
-        format = format
-      )
+  req <- httr2::req_url_query(
+    req,
+    method = method,
+    api_key = api_key
+  )
 
-    if (!is.null(format) && (format == "json")) {
-      req <-
-        httr2::req_url_query(
-          req,
-          nojsoncallback = "1"
-        )
-    }
+  req <- httr2::req_url_query(req, ...)
 
-    resp <- httr2::req_perform(req)
+  req <- httr2::req_url_query(req, format = format)
 
-    format <- match.arg(format, c("json", "xml", "string", "raw"))
-
-    switch(format,
-      "json" = httr2::resp_body_json(resp,
-        check_type = check_type,
-        simplifyVector = simplifyVector
-      ),
-      "xml" = httr2::resp_body_json(resp, check_type = check_type),
-      "string" = httr2::resp_body_string(resp),
-      "raw" = httr2::resp_body_raw(resp)
-    )
+  if (!is.null(format) && (format == "json")) {
+    req <- httr2::req_url_query(req, nojsoncallback = "1")
   }
+
+  resp <- httr2::req_perform(req)
+
+  format <- match.arg(format, c("json", "xml", "string", "raw"))
+
+  switch(format,
+    "json" = httr2::resp_body_json(resp,
+      check_type = check_type,
+      simplifyVector = simplifyVector
+    ),
+    "xml" = httr2::resp_body_json(resp, check_type = check_type),
+    "string" = httr2::resp_body_string(resp),
+    "raw" = httr2::resp_body_raw(resp)
+  )
+}
