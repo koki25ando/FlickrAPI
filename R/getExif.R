3c5aa0bee21f602a6cfc6e2d90812964f5cf4704
Eli Pousson <eli.pousson@gmail.com>
2023-01-16 20:34:43 -0500

    feat(getExif): add support for secret arg

--- R/getExif.R
+++ R/getExif.R
@@ -5,26 +5,32 @@
 #'
 #' @inheritParams FlickrAPIRequest
 #' @param photo_id The id of the photo to fetch information for
-#'
+#' @param secret The secret for the photo. If the correct secret is passed then
+#'   permissions checking is skipped. This enables the 'sharing' of individual
+#'   photos by passing around the id and secret.
 #' @return This function returns a data frame of EXIF information of given
 #'   photograph
 #'
 #' @examples
 #' \dontrun{
 #' getExif(
-#' api_key = get_flickr_api_key(),
-#' photo_id = "45961963324"
+#'   api_key = get_flickr_api_key(),
+#'   photo_id = "45961963324"
 #' )
 #' }
 #'
+#' @seealso
+#' - Flickr API Documentation: [flickr.photos.getExif](https://www.flickr.com/services/api/flickr.photos.getExif.html)
 #' @export
 getExif <- function(api_key = NULL,
-                    photo_id = NULL) {
+                    photo_id = NULL,
+                    secret = NULL) {
   data <-
     FlickrAPIRequest(
       method = "flickr.photos.getExif",
       api_key = api_key,
-      photo_id = photo_id
+      photo_id = photo_id,
+      secret = secret
     )
 
   as.data.frame(data$photo$exif)
