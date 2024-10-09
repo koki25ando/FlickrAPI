40e4a7763523f1a166df1d2764ee7c36cf32321a
Eli Pousson <eli.pousson@gmail.com>
2024-10-08 22:57:03 -0400

    Switch to standard package level docs w/ usethis
    Also add Language to DESCRIPTION

--- R/FlickrAPI-package.R
+++ R/FlickrAPI-package.R
@@ -1,12 +1,12 @@
-#' FlickrAPI: Access to Flickr API via R
-#'
-#' An R package that provides an interface to the Flickr API
-#' <https://www.flickr.com/services/api/>, which allows R users to download
-#' publicly available data on Flickr feed such as location, EXIF, list of photos
-#' posted by specific user.
-#'
-#' @author Koki Ando \email{koki.25.ando@gmail.com}
-#' @docType package
-#' @seealso <https://www.flickr.com/services/api/>
 #' @keywords internal
+#' @details
+#' The Flickr API <https://www.flickr.com/services/api/>, which allows R users
+#' to download publicly available data on Flickr feed such as location, EXIF,
+#' list of photos posted by specific user.
+#'
 "_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rlang caller_env arg_match
+## usethis namespace: end
+NULL
